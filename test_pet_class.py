78e9014bf9c46bd86bf69651adfd593081e35d88
jbboncay17 <jbboncay1@gmail.com>
2026-06-03 21:57:26 +0800

    complete variables for import file

--- test_pet_class.py
+++ test_pet_class.py
@@ -5,3 +5,12 @@ pet = Pet()
 name = input("Enter pet name: ")
 animal_type = input("Enter animal type: ")
 age = int(input("Enter pet age: "))
+
+pet.set_name(name)
+pet.set_animal_type(animal_type)
+pet.set_age(age)
+
+print("\nPet Information")
+print("Name:", pet.get_name())
+print("Type:", pet.get_animal_type())
+print("Age:", pet.get_age())
