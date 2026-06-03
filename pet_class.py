0cb82e295dbd22f8c23d53e3feb01afc427fbe62
jbboncay17 <jbboncay1@gmail.com>
2026-06-03 21:53:09 +0800

    changed the arrangement

--- pet_class.py
+++ pet_class.py
@@ -1,11 +1,15 @@
 class Pet:
     def __init__(self):
         self.__name = ""
-        self.__animal_type = ""
         self.__age= 0
+        self.__animal_type = ""
+
 
     def set_name(self,name):
         self.__name = name
 
-    def set_animal_type(self,animal_type):
+    def set_age(self,age):
+        self.__age = age
+
+    def set_animal_type(self, animal_type):
         self.__animal_type = animal_type
