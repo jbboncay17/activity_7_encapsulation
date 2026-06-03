d72e98eb94217c9e59adcaddc76d721ec7c57e4e
jbboncay17 <jbboncay1@gmail.com>
2026-06-03 21:49:53 +0800

    Identify class and init

---
+++ pet_class.py
@@ -0,0 +1,11 @@
+class Pet:
+    def __init__(self):
+        self.__name = ""
+        self.__animal_type = ""
+        self.__age= 0
+
+    def set_name(self,name):
+        self.__name = name
+
+    def set_animal_type(self,animal_type):
+        self.__animal_type = animal_type
