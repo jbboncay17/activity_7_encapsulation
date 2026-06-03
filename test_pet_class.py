9317c70d98dc0b5e988297e2a429cbde6e24c01d
jbboncay17 <jbboncay1@gmail.com>
2026-06-03 22:12:27 +0800

    final changes, arrangement, and added colors and designs

--- test_pet_class.py
+++ test_pet_class.py
@@ -1,16 +1,35 @@
-from pet_class import Pet
+bold="\033[1m"
+red="\033[31m"
+end="\033[0m"
+green="\033[32m"
+blue="\033[34m"
+yellow="\033[33m"
+purple="\033[35m"
 
+
+from pet_class import Pet
+print("")
+print(bold+purple+"************ PET CLASS TEST ************"+end)
 pet = Pet()
 
-name = input("Enter pet name: ")
-animal_type = input("Enter animal type: ")
-age = int(input("Enter pet age: "))
-
+name = input(bold+blue+"Enter pet name: "+end)
+animal_type = input(bold+blue+"Enter animal type: "+end)
+sex=input(bold+blue+"Male/Female: "+end)
+animal_breed = input (bold+blue+"Enter animal breed: "+end)
+age = int(input(bold+blue+"Enter pet age: "+end))
+print(bold+purple+"****************************************"+end)
+print("")
 pet.set_name(name)
 pet.set_animal_type(animal_type)
+pet.set_animal_breed(animal_breed)
+pet.set_sex(sex)
 pet.set_age(age)
 
-print("\nPet Information")
-print("Name:", pet.get_name())
-print("Type:", pet.get_animal_type())
-print("Age:", pet.get_age())
+
+print(bold+yellow+"************ PET INFORMATION ************"+end)
+print(bold+green+"Name:"+end, pet.get_name())
+print(bold+green+"Type:"+end, pet.get_animal_type())
+print(bold+green+"Sex:"+end, pet.get_sex())
+print(bold+green+"Breed:"+end, pet.get_animal_breed())
+print(bold+green+"Age:"+end, pet.get_age())
+print(bold+yellow+"*****************************************"+end)
