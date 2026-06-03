afccb4284327166914160c99be01c408b67c85eb
jbboncay17 <jbboncay1@gmail.com>
2026-06-03 21:30:26 +0800

    finished the import file

--- test_car_class.py
+++ test_car_class.py
@@ -3,3 +3,16 @@ from car_class import Car
 def test_car():
     car = Car(2023,Toyota):
     return car
+
+if __name__ == "__main__":
+    car = test_car()
+
+    print("Accelerating:")
+    for i in range(5):
+        car.accelerate()
+        print("Current Speed:", car.get_speed())
+
+    print("\nBraking:")
+    for i in range(5):
+        car.brake()
+        print("Current Speed:", car.get_speed())
